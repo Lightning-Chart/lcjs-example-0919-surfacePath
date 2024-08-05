8fac0d33f191e0924cfdc2b689c702f70b73e120
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-08-05 10:06:37 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,73 @@
+# JavaScript 3D Surface Path Projection 2D
+
+![JavaScript 3D Surface Path Projection 2D](surfacePath-darkGold.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
+
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
+
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+Simple example of custom user interaction in a 3D surface chart.
+
+The application tracks user interactions to the 3D surface data.
+After user has clicked on two locations along the surface, the application highlights the path between those 3D points along the surface.
+
+The same data is also displayed in a 2D chart below.
+
+
+## API Links
+
+* [3D chart]
+* [3D axis]
+* [Paletted Fill]
+* [Color lookup table]
+* [Color RGBA factory]
+* [Color shading styles (3D)]
+* [Surface Grid Series]
+* [Heatmap Grid Series]
+* [Legend Box]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [LightningChart][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact sales@lightningchart.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
+
+[0]: https://github.com/Arction/
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://lightningchart.com/support-services/
+
+© LightningChart Ltd 2009-2022. All rights reserved.
+
+
+[3D chart]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/Chart3D.html
+[3D axis]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/Axis3D.html
+[Paletted Fill]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/PalettedFill.html
+[Color lookup table]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/LUT.html
+[Color RGBA factory]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/functions/ColorRGBA.html
+[Color shading styles (3D)]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/variables/ColorShadingStyles.html
+[Surface Grid Series]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/SurfaceGridSeries3D.html
+[Heatmap Grid Series]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/HeatmapGridSeriesIntensityValues.html
+[Legend Box]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/Chart.html#addLegendBox
+
---
+++ src/index.js
@@ -0,0 +1,136 @@
+/**
+ * Example showcasing the Surface Grid Series feature of LightningChart JS.
+ */
+// Import LightningChartJS
+const lcjs = require('@lightningchart/lcjs')
+
+// Import xydata
+const xydata = require('@lightningchart/xydata')
+
+const { lightningChart, LUT, PalettedFill, emptyLine, ColorShadingStyles, LegendBoxBuilders, regularColorSteps, SolidLine, Themes } = lcjs
+const { createWaterDropDataGenerator } = xydata
+
+const HEATMAP_COLUMNS = 500
+const HEATMAP_ROWS = 500
+
+const exampleContainer = document.getElementById('chart') || document.body
+if (exampleContainer === document.body) {
+    exampleContainer.style.width = '100vw'
+    exampleContainer.style.height = '100vh'
+    exampleContainer.style.margin = '0px'
+}
+const containerChart1 = document.createElement('div')
+const containerChart2 = document.createElement('div')
+exampleContainer.append(containerChart1)
+exampleContainer.append(containerChart2)
+containerChart1.style.width = '100%'
+containerChart1.style.height = '70%'
+containerChart2.style.width = '100%'
+containerChart2.style.height = '30%'
+
+const lc = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
+const chart3D = lc
+    .Chart3D({
+        container: containerChart1,
+        theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
+    })
+    .setTitle('Click twice to project path to 2D')
+    .setBoundingBox({ x: 1, z: 1, y: 0.4 })
+    .setCameraAutomaticFittingEnabled(false)
+    .setCameraLocation({ x: 0.23, z: 0.44, y: 0.5 })
+    .setCursorMode(undefined)
+
+const chart2D = lc
+    .ChartXY({
+        container: containerChart2,
+        // theme: Themes.darkGold
+    })
+    .setTitle('')
+chart2D.forEachAxis((axis) => axis.setAnimationScroll(false))
+chart2D.axisY.setInterval({ start: 0, end: 100 })
+chart2D.axisX.setTitle('Distance')
+
+createWaterDropDataGenerator()
+    .setColumns(HEATMAP_COLUMNS)
+    .setRows(HEATMAP_ROWS)
+    .generate()
+    .then((data) => {
+        const theme = chart3D.getTheme()
+        const lut = new LUT({
+            interpolate: true,
+            steps: regularColorSteps(0, 70, theme.examples.coldHotColorPalette),
+        })
+        const palettedFill = new PalettedFill({ lut, lookUpProperty: 'y' })
+
+        const surfaceSeries3D = chart3D
+            .addSurfaceGridSeries({
+                columns: HEATMAP_COLUMNS,
+                rows: HEATMAP_ROWS,
+            })
+            .setFillStyle(palettedFill)
+            .setWireframeStyle(emptyLine)
+            .setColorShadingStyle(new ColorShadingStyles.Phong())
+            .invalidateHeightMap(data)
+
+        const projection2D = chart2D.addPointLineAreaSeries({ dataPattern: 'ProgressiveX' }).setAreaFillStyle(palettedFill)
+
+        chart2D.axisY.setStrokeStyle(new SolidLine({ thickness: 3, fillStyle: palettedFill }))
+
+        const legend = chart3D.addLegendBox(LegendBoxBuilders.VerticalLegendBox).add(chart3D)
+
+        // Custom interaction;
+        // Click to draw path along 3D surface heatmap.
+        let pointA
+        let pointB
+        const pathSeries = chart3D
+            .addPointSeries({ individualPointSizeEnabled: true })
+            .setMouseInteractions(false)
+            .setAutoScrollingEnabled(false)
+        const updtPath = () => {
+            const points = []
+            const points2D = []
+            if (pointA) {
+                points.push({ ...pointA, size: 15 })
+            }
+            if (pointA && pointB) {
+                points2D.push({ x: 0, y: pointA.y })
+                const dX = pointB.x - pointA.x
+                const dZ = pointB.z - pointA.z
+                const length = Math.sqrt(dX ** 2 + dZ ** 2)
+                const dir = { x: dX / length, z: dZ / length }
+                const stepXZ = 5
+                for (let pos = stepXZ; pos <= length - stepXZ; pos += stepXZ) {
+                    const x = pointA.x + pos * dir.x
+                    const z = pointA.z + pos * dir.z
+                    const y = data[Math.round(x)][Math.round(z)]
+                    points.push({ x, z, y, size: 8 })
+                    points2D.push({ x: pos, y })
+                }
+                points.push({ ...pointB, size: 15 })
+                points2D.push({ x: length, y: pointB.y })
+            }
+            pathSeries.clear().add(points)
+            projection2D.clear().appendJSON(points2D)
+        }
+        surfaceSeries3D.onMouseMove((_, event, hit) => {
+            if (!pointA || pointA.preview) {
+                pointA = { ...hit, preview: true }
+            } else if (!pointB || pointB.preview) {
+                pointB = { ...hit, preview: true }
+            }
+            updtPath()
+        })
+        surfaceSeries3D.onMouseClick((_, event, hit) => {
+            if (!pointA || pointA.preview) {
+                pointA = hit
+            } else if (!pointB || pointB.preview) {
+                pointB = hit
+            } else {
+                pointA = undefined
+                pointB = undefined
+            }
+            updtPath()
+        })
+    })
